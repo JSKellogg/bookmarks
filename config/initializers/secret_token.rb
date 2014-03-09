db483f4d9b60d8a1b73e00baf4f8a81934c8cb82
dalia <abuadas.dalia@gmail.com>
2014-03-09 12:30:48 -0400

    adding bookmarks project to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookmarks::Application.config.secret_key_base = '04466e19b61a718963d03ebd2cbc69841c8446044cbba936188d70e397dca31a08108fca9f5b6c5fda9359399304a1585e6feafa3005a56926634bbc97484516'
